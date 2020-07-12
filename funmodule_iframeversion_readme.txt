d5e0d8eaa5a0d5afa8b1ef62e5670f4aaa86b58f
mambax7 <mambax7@gmail.com>
2020-07-11 22:45:39 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ funmodule_iframeversion_readme.txt
@@ -0,0 +1,13 @@
+Tested on Xoops 2.0.7 (stable)
+
+
+
+Extraxt in {XOOPS_ROOT}/modules/
+
+go to admin-panel an install it.
+
+Have Phun!
+
+
+______________________________
+Suggestions? Write to nastytunez@gmx.de
---
+++ xoops_version.php
@@ -0,0 +1,43 @@
+<?php
+// $Id: xoops_version.php,v 1.1 2004/01/29 14:45:56 buennagel Exp $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+$modversion['name'] = _MI_FUNVIDEOS_NAME;
+$modversion['version'] = 1.00;
+$modversion['description'] = _MI_FUNVIDEOS_DESC;
+$modversion['credits'] = "www.freegaming.de";
+$modversion['author'] = "Brian Ferris";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/slogo.png";
+$modversion['dirname'] = "funvideos";
+// Admin
+$modversion['hasAdmin'] = 0;
+$modversion['adminmenu'] = '';
+// Menu
+$modversion['hasMain'] = 1;
+
+?>
